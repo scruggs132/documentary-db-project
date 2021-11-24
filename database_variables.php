0fd4b183075d7343b51c7fc6a38c482287e999f4
Rena Wolinsky <rjw3kk@virginia.edu>
2021-11-24 00:21:29 -0500

    using github on atom for first time, want to make sure im doing it right

--- database_variables.php
+++ database_variables.php
@@ -1,5 +1,6 @@
 <?php
 // local variables
+//think we can take this file out when finishing up but I'm just going to leave it in here for now
 $SERVER   = 'localhost:3306';
 $USERNAME = 'reen2022';
 $PASSWORD = 'HappinessIsOverrated11!';
--- readme.txt
+++ readme.txt
@@ -1,2 +1,2 @@
 Save this repo in C:\xampp\htdocs
-We will connect to a publically hosted database but run the server locally from XApp
+We will connect to a publicly hosted database but run the server locally from XAMPP 
