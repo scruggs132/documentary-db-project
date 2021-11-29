2a8c67d67e14baabd2095b98313b01dd960848c0
Cris Scruggs <cms3zd@virginia.edu>
2021-11-28 22:12:59 -0500

    changed id references to userID

--- local-config.php
+++ local-config.php
@@ -2,9 +2,9 @@
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'localhost:3306');
-define('DB_USERNAME', 'reen2022');
-define('DB_PASSWORD', 'HappinessIsOverrated11!');
-define('DB_NAME', 'proj');
+define('DB_USERNAME', 'cris');
+define('DB_PASSWORD', 'password');
+define('DB_NAME', 'doc-proj-test');
 
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
